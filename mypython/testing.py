22d2750d4b0e8762a6595c19aa4539b1f87079d5
Roberto Aguiar Lima <aguiarlima.roberto@gmail.com>
2015-01-07 15:15:06 -0200

    violent

---
+++ mypython/testing.py
@@ -0,0 +1,25 @@
+function xor()
+
+	x = [0 0; 1 0; 0 1; 1 1];
+
+	y = [0 1 1 0];
+
+	a1 = [[1 1 1 1], x];
+
+	theta1 = zeros(3,2);
+	
+	z2 = a1 * theta1;
+	a2 = 1/(1+exp(-z2));
+	a2 = [[1 1 1 1], a2];
+
+	theta2 = zeros(3, 1);
+	z3 = a2 * theta3;
+	a3 = 1/(1+exp(-z3));
+
+	ldelta3 = 	
+	
+	
+end
+
+	
+xor()
